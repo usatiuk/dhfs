bcbcd4151ca8ba5205a3ae98d3f81b755e4670c4
Stepan Usatiuk <stepan@usatiuk.com>
2024-06-22 14:58:16 +0200

    a little cleanup 2

--- server/src/main/java/com/usatiuk/dhfs/storage/files/objects/NotImplementedConflictResolver.java
+++ server/src/main/java/com/usatiuk/dhfs/storage/files/objects/NotImplementedConflictResolver.java
@@ -1,10 +1,11 @@
 package com.usatiuk.dhfs.storage.files.objects;
 
-import com.usatiuk.dhfs.objects.repository.distributed.ObjectHeader;
 import com.usatiuk.dhfs.storage.objects.jrepository.JObject;
 import com.usatiuk.dhfs.storage.objects.repository.distributed.ConflictResolver;
+import jakarta.enterprise.context.ApplicationScoped;
 import org.apache.commons.lang3.NotImplementedException;
 
+@ApplicationScoped
 public class NotImplementedConflictResolver implements ConflictResolver {
     @Override
     public ConflictResolutionResult resolve(String conflictHost, JObject<?> conflictSource) {
