223baac2e9b318a2d4e80e015847ab7c52e0ed23
Stepan Usatiuk <stepan@usatiuk.com>
2024-06-22 18:01:22 +0200

    broadcast receive fix 5

--- server/src/main/java/com/usatiuk/dhfs/storage/objects/repository/distributed/peersync/PeerSyncServer.java
+++ server/src/main/java/com/usatiuk/dhfs/storage/objects/repository/distributed/peersync/PeerSyncServer.java
@@ -1,15 +1,34 @@
 package com.usatiuk.dhfs.storage.objects.repository.distributed.peersync;
 
 import com.usatiuk.dhfs.objects.repository.distributed.peersync.DhfsObjectPeerSyncGrpc;
+import com.usatiuk.dhfs.objects.repository.distributed.peersync.PeerInfo;
 import com.usatiuk.dhfs.objects.repository.distributed.peersync.SyncPeersData;
+import com.usatiuk.dhfs.storage.objects.repository.distributed.HostInfo;
+import com.usatiuk.dhfs.storage.objects.repository.distributed.PersistentRemoteHostsService;
 import io.quarkus.grpc.GrpcService;
+import io.smallrye.common.annotation.Blocking;
 import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
 
 @GrpcService
 public class PeerSyncServer implements DhfsObjectPeerSyncGrpc {
+    @Inject
+    PersistentRemoteHostsService persistentRemoteHostsService;
 
     @Override
+    @Blocking
     public Uni<SyncPeersData> syncPeers(SyncPeersData request) {
-        return null;
+        var builder = SyncPeersData.newBuilder();
+        builder.setSelfUuid(persistentRemoteHostsService.getSelfUuid().toString());
+        for (var h : persistentRemoteHostsService.getHosts()) {
+            builder.addMyPeers(h.toPeerInfo());
+        }
+        builder.addMyPeers(PeerInfo.newBuilder().setUuid(persistentRemoteHostsService.getSelfUuid().toString()).build());
+
+        for (var np : request.getMyPeersList()) {
+            persistentRemoteHostsService.addHost(new HostInfo(np.getUuid()));
+        }
+
+        return Uni.createFrom().item(builder.build());
     }
 }
