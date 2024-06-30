fe9384d9b4cf143d65a733b05f34ffab22d89be8
Stepan Usatiuk <stepan@usatiuk.com>
2024-06-30 21:00:05 +0200

    logging in tests fix

--- server/src/test/java/com/usatiuk/dhfs/integration/DhfsFusex3IT.java
+++ server/src/test/java/com/usatiuk/dhfs/integration/DhfsFusex3IT.java
@@ -49,7 +49,8 @@ public class DhfsFusex3IT {
                                         "-Ddhfs.objects.distributed.invalidation.delay=100",
                                         "-Djava.util.concurrent.ForkJoinPool.common.parallelism=4",
                                         "-Ddhfs.objects.ref_verification=true",
-                                        "-Dquarkus.log.category.\\\"com.usatiuk.dhfs\\\".level=TRACE",
+                                        "-Dquarkus.log.category.\"com.usatiuk.dhfs\".level=TRACE",
+                                        "-Dquarkus.log.category.\"com.usatiuk.dhfs\".min-level=TRACE",
                                         "-jar", "/app/quarkus-run.jar")
                                 .build())
                 .withFileFromPath("/app", Paths.get(buildPath, "quarkus-app"));
