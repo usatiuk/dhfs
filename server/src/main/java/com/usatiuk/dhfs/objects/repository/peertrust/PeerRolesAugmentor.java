c27477c7a0e4ed1292b71fc9fb1875ad1b9e0a3a
Stepan Usatiuk <stepan@usatiuk.com>
2024-07-09 21:58:44 +0200

    hack

--- server/src/main/java/com/usatiuk/dhfs/objects/repository/peertrust/PeerRolesAugmentor.java
+++ server/src/main/java/com/usatiuk/dhfs/objects/repository/peertrust/PeerRolesAugmentor.java
@@ -1,7 +1,6 @@
 package com.usatiuk.dhfs.objects.repository.peertrust;
 
 import com.usatiuk.dhfs.objects.repository.PersistentRemoteHostsService;
-import io.quarkus.logging.Log;
 import io.quarkus.security.credential.CertificateCredential;
 import io.quarkus.security.identity.AuthenticationRequestContext;
 import io.quarkus.security.identity.SecurityIdentity;
@@ -11,7 +10,6 @@ import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
-import java.util.UUID;
 import java.util.function.Supplier;
 
 @ApplicationScoped
@@ -30,22 +28,31 @@ public class PeerRolesAugmentor implements SecurityIdentityAugmentor {
         } else {
             QuarkusSecurityIdentity.Builder builder = QuarkusSecurityIdentity.builder(identity);
 
-            var uuid = identity.getPrincipal().getName().substring(3);
-
-            try {
-                var entry = persistentRemoteHostsService.getHost(UUID.fromString(uuid));
-
-                if (!entry.getCertificate().equals(identity.getCredential(CertificateCredential.class).getCertificate())) {
-                    Log.error("Certificate mismatch for " + uuid);
-                    return () -> identity;
-                }
-
+            // FIXME: The below is just an additional security check, we still check the certificates
+            // with the normal TLS mechanisms.
+            // But my guess is there's a race condition between tls store update and quarkus checking this somehow?
+            // So the anonymous identity gets cached for a channel and it returns UNAUTHORIZED all the time...
+            if (identity.getCredential(CertificateCredential.class).getCertificate() != null) {
                 builder.addRole("cluster-member");
                 return builder::build;
-            } catch (Exception e) {
-                Log.error("Error when checking certificate for " + uuid, e);
-                return () -> identity;
             }
+            return () -> identity;
+//            var uuid = identity.getPrincipal().getName().substring(3);
+//
+//            try {
+//                var entry = persistentRemoteHostsService.getHost(UUID.fromString(uuid));
+//
+//                if (!entry.getCertificate().equals(identity.getCredential(CertificateCredential.class).getCertificate())) {
+//                    Log.error("Certificate mismatch for " + uuid);
+//                    return () -> identity;
+//                }
+//
+//                builder.addRole("cluster-member");
+//                return builder::build;
+//            } catch (Exception e) {
+//                Log.error("Error when checking certificate for " + uuid, e);
+//                return () -> identity;
+//            }
         }
     }
 }
