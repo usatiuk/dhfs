bacf7307f2b5380737ab4001dec17628c42266bf
Stepan Usatiuk <stepan@usatiuk.com>
2024-07-27 23:20:47 +0200

    fixup defaults in scripts

--- dockerentry.sh
+++ dockerentry.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+set -e || true
+set -u || true
+set -o pipefail || true
+set -x || true
+
 exec java \
     --add-exports java.base/sun.nio.ch=ALL-UNNAMED \
     -Ddhfs.objects.persistence.files.root=/dhfs_root/p \
     -Ddhfs.objects.root=/dhfs_root/d \
     -Ddhfs.fuse.root=/dhfs_root_fuse \
     -Dquarkus.http.host=0.0.0.0 \
+    -Ddhfs.objects.ref_verification=false \
+    -Dquarkus.log.category.\"com.usatiuk.dhfs\".level=$DHFS_LOGLEVEL \
     "$@" \
     -jar quarkus-run.jar
