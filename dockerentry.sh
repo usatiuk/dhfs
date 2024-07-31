8e33dae3037697c43053295610da79723f5d460c
Stepan Usatiuk <stepan@usatiuk.com>
2024-07-31 18:16:15 +0200

    fix docker/run-wrapper

--- dockerentry.sh
+++ dockerentry.sh
@@ -8,6 +8,7 @@ set -x || true
 exec java \
     -Xmx512M \
     --add-exports java.base/sun.nio.ch=ALL-UNNAMED \
+    --add-exports java.base/jdk.internal.access=ALL-UNNAMED \
     -Ddhfs.objects.persistence.files.root=/dhfs_root/p \
     -Ddhfs.objects.root=/dhfs_root/d \
     -Ddhfs.fuse.root=/dhfs_root_fuse \
