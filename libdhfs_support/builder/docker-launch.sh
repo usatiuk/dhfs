10d384b614bc777b0889028afc5c64848a06d6ab
Stepan Usatiuk <stepan@usatiuk.com>
2024-08-23 20:04:35 +0200

    fix

--- libdhfs_support/builder/docker-launch.sh
+++ libdhfs_support/builder/docker-launch.sh
@@ -10,9 +10,9 @@ export SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd
 cd "$SCRIPT_DIR"
 
 DOCKER_IMG_FILE=$(mktemp)
-docker build --iidfile "$DOCKER_IMG_FILE" .
+docker build $PLATFORM_ARG --iidfile "$DOCKER_IMG_FILE" .
 
 ROOT_DIR="$(dirname "$(dirname "$SCRIPT_DIR")")"
 
-docker run --rm -v "$ROOT_DIR:$ROOT_DIR" -e INSIDE_DOCKER_ALREADY=TRUE "$(cat "$DOCKER_IMG_FILE")" \
+docker run $PLATFORM_ARG --rm -v "$ROOT_DIR:$ROOT_DIR" -e INSIDE_DOCKER_ALREADY=TRUE "$(cat "$DOCKER_IMG_FILE")" \
   "$SCRIPT_DIR/cross-build.sh" "$@"
