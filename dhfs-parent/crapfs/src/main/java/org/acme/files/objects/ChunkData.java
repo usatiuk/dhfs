14ba4b8e2eb64484c6b25748e0b0607b8fdb497f
Stepan Usatiuk <stepan@usatiuk.com>
2024-12-18 13:30:32 +0100

    revert crapfs

--- dhfs-parent/crapfs/src/main/java/org/acme/files/objects/ChunkData.java
+++
@@ -1,9 +0,0 @@
-package org.acme.files.objects;
-
-import com.usatiuk.objects.common.runtime.JData;
-
-public interface ChunkData extends JData {
-    byte[] getData();
-
-    void setData(byte[] data);
-}
