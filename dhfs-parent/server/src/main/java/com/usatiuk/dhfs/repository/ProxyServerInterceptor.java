312cf18b27d356254413a3416b4dd05a39ef8be7
Stepan Usatiuk <stepan@usatiuk.com>
2025-03-28 12:02:20 +0100

    Server: hopefully working reverse proxy connection

--- dhfs-parent/server/src/main/java/com/usatiuk/dhfs/repository/ProxyServerInterceptor.java
+++ dhfs-parent/server/src/main/java/com/usatiuk/dhfs/repository/ProxyServerInterceptor.java
@@ -17,10 +17,13 @@ public class ProxyServerInterceptor implements ServerInterceptor {
         }
 
         if (metadata.containsKey(PROXY_TO_HEADER_KEY)) {
-            context = Context.current().withValue(PROXY_TO_HEADER_KEY_CTX, metadata.get(PROXY_TO_HEADER_KEY));
+            var socketAddress = serverCall.getAttributes().get(Grpc.TRANSPORT_ATTR_REMOTE_ADDR);
+            context = Context.current().withValue(PROXY_TO_HEADER_KEY_CTX, metadata.get(PROXY_TO_HEADER_KEY))
+                    .withValue(PROXY_TO_FROM_ADDR_KEY_CTX, socketAddress);
         } else if (metadata.containsKey(PROXY_FROM_HEADER_KEY)) {
             context = Context.current().withValue(PROXY_FROM_HEADER_KEY_CTX, metadata.get(PROXY_FROM_HEADER_KEY));
         }
+
 
         if (context != null) {
             return Contexts.interceptCall(
--- dhfs-parent/server/src/main/java/com/usatiuk/dhfs/repository/RemoteObjectServiceServer.java
+++ dhfs-parent/server/src/main/java/com/usatiuk/dhfs/repository/RemoteObjectServiceServer.java
@@ -1,6 +1,9 @@
 package com.usatiuk.dhfs.repository;
 
 import com.usatiuk.dhfs.PeerId;
+import com.usatiuk.dhfs.repository.peerdiscovery.IpPeerAddress;
+import com.usatiuk.dhfs.repository.peerdiscovery.PeerAddressType;
+import com.usatiuk.dhfs.repository.peerdiscovery.PeerDiscoveryDirectory;
 import io.quarkus.grpc.GrpcService;
 import io.quarkus.grpc.RegisterInterceptor;
 import io.quarkus.logging.Log;
@@ -10,13 +13,14 @@ import io.smallrye.mutiny.Uni;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 
+import java.net.InetSocketAddress;
 import java.util.Optional;
 import java.util.function.Function;
 
-import static com.usatiuk.dhfs.repository.ProxyConstants.PROXY_FROM_HEADER_KEY_CTX;
-import static com.usatiuk.dhfs.repository.ProxyConstants.PROXY_TO_HEADER_KEY_CTX;
+import static com.usatiuk.dhfs.repository.ProxyConstants.*;
 
 // Note: RunOnVirtualThread hangs somehow
+
 @GrpcService
 @RolesAllowed("cluster-member")
 @RegisterInterceptor(ProxyServerInterceptor.class)
@@ -29,7 +33,8 @@ public class RemoteObjectServiceServer implements DhfsObjectSyncGrpc {
     RpcClientFactory rpcClientFactory;
     @Inject
     PeerManager peerManager;
-
+    @Inject
+    PeerDiscoveryDirectory peerDiscoveryDirectory;
 
     private PeerId getIdentity() {
         if (PROXY_FROM_HEADER_KEY_CTX.get() != null) {
@@ -49,6 +54,21 @@ public class RemoteObjectServiceServer implements DhfsObjectSyncGrpc {
     private <T> Optional<Uni<T>> tryProxy(Function<DhfsObjectSyncGrpcGrpc.DhfsObjectSyncGrpcBlockingStub, T> fn) {
         var proxyTarget = getProxyTarget();
         if (proxyTarget != null) {
+            var fromAddr = PROXY_TO_FROM_ADDR_KEY_CTX.get();
+            if (fromAddr instanceof InetSocketAddress inetAddr) {
+                peerDiscoveryDirectory.notifyAddr(
+                        new IpPeerAddress(
+                                PeerId.of(identity.getPrincipal().getName().substring(3)),
+                                PeerAddressType.WAN,
+                                inetAddr.getAddress(),
+                                -1,
+                                inetAddr.getPort()
+                        )
+                );
+            } else {
+                Log.warnv("Expected InetSocketAddress but got {0}", fromAddr);
+            }
+
             return Optional.of(Uni.createFrom().item(rpcClientFactory.<T>withObjSyncClient(
                     proxyTarget,
                     getIdentity(),
