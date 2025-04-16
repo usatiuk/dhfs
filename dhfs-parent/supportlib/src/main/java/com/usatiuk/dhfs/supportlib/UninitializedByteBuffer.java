94218330b11f151d9449e9a7c7180ab929c9133a
Stepan Usatiuk <stepan@usatiuk.com>
2025-04-16 16:26:58 +0200

    Simplify allocateUninitialized

--- dhfs-parent/supportlib/src/main/java/com/usatiuk/dhfs/supportlib/UninitializedByteBuffer.java
+++ dhfs-parent/supportlib/src/main/java/com/usatiuk/dhfs/supportlib/UninitializedByteBuffer.java
@@ -9,24 +9,17 @@ public class UninitializedByteBuffer {
     private static final Logger LOGGER = Logger.getLogger(UninitializedByteBuffer.class.getName());
 
     public static ByteBuffer allocateUninitialized(int size) {
-        try {
-            if (size < DhfsSupport.PAGE_SIZE)
-                return ByteBuffer.allocateDirect(size);
-
-            var bb = new ByteBuffer[1];
-            long token = DhfsSupport.allocateUninitializedByteBuffer(bb, size);
-            var ret = bb[0];
-            CLEANER.register(ret, () -> {
-                try {
-                    DhfsSupport.releaseByteBuffer(token);
-                } catch (Throwable e) {
-                    LOGGER.severe("Error releasing buffer: " + e);
-                    System.exit(-1);
-                }
-            });
-            return ret;
-        } catch (OutOfMemoryError e) {
-            return ByteBuffer.allocate(size);
-        }
+        var bb = new ByteBuffer[1];
+        long token = DhfsSupport.allocateUninitializedByteBuffer(bb, size);
+        var ret = bb[0];
+        CLEANER.register(ret, () -> {
+            try {
+                DhfsSupport.releaseByteBuffer(token);
+            } catch (Throwable e) {
+                LOGGER.severe("Error releasing buffer: " + e);
+                System.exit(-1);
+            }
+        });
+        return ret;
     }
 }
