f6685f45f947bc1e2c50bbcacbe542b91413b58b
Stepan Usatiuk <stepan@usatiuk.com>
2025-04-24 22:34:04 +0200

    Dhfs-app: increase sync timeout

--- dhfs-parent/dhfs-app/src/test/java/com/usatiuk/dhfs/integration/DhfsImage.java
+++ dhfs-parent/dhfs-app/src/test/java/com/usatiuk/dhfs/integration/DhfsImage.java
@@ -66,7 +66,7 @@ public class DhfsImage implements Future<String> {
                                 .run("apt update && apt install -y libfuse2 curl gcc")
                                 .copy("/app", "/app")
                                 .copy("/libs", "/libs")
-                                .cmd("java", "-ea", "-Xmx256M", "-XX:TieredStopAtLevel=1","-XX:+UseParallelGC",
+                                .cmd("java", "-ea", "-Xmx256M", "-XX:TieredStopAtLevel=1", "-XX:+UseParallelGC",
                                         "--add-exports", "java.base/sun.nio.ch=ALL-UNNAMED",
                                         "--add-exports", "java.base/jdk.internal.access=ALL-UNNAMED",
                                         "--add-opens=java.base/java.nio=ALL-UNNAMED",
@@ -75,7 +75,7 @@ public class DhfsImage implements Future<String> {
                                         "-Ddhfs.objects.deletion.delay=0",
                                         "-Ddhfs.objects.deletion.can-delete-retry-delay=1000",
                                         "-Ddhfs.objects.ref_verification=true",
-                                        "-Ddhfs.objects.sync.timeout=10",
+                                        "-Ddhfs.objects.sync.timeout=30",
                                         "-Ddhfs.objects.sync.ping.timeout=5",
                                         "-Ddhfs.objects.reconnect_interval=1s",
                                         "-Dquarkus.log.category.\"com.usatiuk\".level=TRACE",
