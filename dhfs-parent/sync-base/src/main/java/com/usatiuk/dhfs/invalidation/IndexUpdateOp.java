db51d7280c7c7cfb46b40abc5b84c91efbf9da8e
Stepan Usatiuk <stepan@usatiuk.com>
2025-05-03 11:25:23 +0200

    Revert "Sync-base: get rid of JDataRemotePush"
    This reverts commit 07133a71

--- dhfs-parent/sync-base/src/main/java/com/usatiuk/dhfs/invalidation/IndexUpdateOp.java
+++ dhfs-parent/sync-base/src/main/java/com/usatiuk/dhfs/invalidation/IndexUpdateOp.java
@@ -1,13 +1,14 @@
 package com.usatiuk.dhfs.invalidation;
 
 import com.usatiuk.dhfs.peersync.PeerId;
+import com.usatiuk.dhfs.remoteobj.JDataRemoteDto;
 import com.usatiuk.objects.JObjectKey;
 import org.pcollections.PMap;
 
 import java.util.Collection;
 import java.util.List;
 
-public record IndexUpdateOp(JObjectKey key, PMap<PeerId, Long> changelog) implements Op {
+public record IndexUpdateOp(JObjectKey key, PMap<PeerId, Long> changelog, JDataRemoteDto data) implements Op {
     @Override
     public Collection<JObjectKey> getEscapedRefs() {
         return List.of(key);
--- dhfs-parent/sync-base/src/main/java/com/usatiuk/dhfs/peersync/PeerInfo.java
+++ dhfs-parent/sync-base/src/main/java/com/usatiuk/dhfs/peersync/PeerInfo.java
@@ -4,10 +4,12 @@ import com.google.protobuf.ByteString;
 import com.usatiuk.dhfs.peertrust.CertificateTools;
 import com.usatiuk.dhfs.remoteobj.JDataRemote;
 import com.usatiuk.dhfs.remoteobj.JDataRemoteDto;
+import com.usatiuk.dhfs.remoteobj.JDataRemotePush;
 import com.usatiuk.objects.JObjectKey;
 
 import java.security.cert.X509Certificate;
 
+@JDataRemotePush
 public record PeerInfo(JObjectKey key, PeerId id, ByteString cert) implements JDataRemote, JDataRemoteDto {
     public PeerInfo(PeerId id, byte[] cert) {
         this(id.toJObjectKey(), id, ByteString.copyFrom(cert));
