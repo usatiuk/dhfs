53ba9dc12e5698ba369d96a3e2252c0ed4afd80a
Stepan Usatiuk <stepan@usatiuk.com>
2025-06-24 17:33:43 +0200

    a start button

--- launcher/gui/src/LauncherAppMainFrame.cpp
+++ launcher/gui/src/LauncherAppMainFrame.cpp
@@ -1,5 +1,12 @@
 #include "LauncherAppMainFrame.h"
 
+#include <iostream>
+
 LauncherAppMainFrame::LauncherAppMainFrame(wxWindow* parent)
     : MainFrame(parent) {
+}
+
+void LauncherAppMainFrame::OnStartStopButtonClick(wxCommandEvent& event) {
+    std::cout << "Hi!" << std::endl;
+    // TODO: Implement OnStartStopButtonClick
 }
