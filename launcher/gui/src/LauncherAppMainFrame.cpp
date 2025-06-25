49b16cda2ff750ac3dac44dae3779ad361713241
Stepan Usatiuk <stepan@usatiuk.com>
2025-06-25 15:44:45 +0200

    webview jank

--- launcher/gui/src/LauncherAppMainFrame.cpp
+++ launcher/gui/src/LauncherAppMainFrame.cpp
@@ -10,6 +10,15 @@ LauncherAppMainFrame::LauncherAppMainFrame(wxWindow* parent)
     : MainFrame(parent) {
     m_javaHomeDirPicker->SetPath(wxFileConfig::Get()->Read(kJavaHomeSettingsKey));
     m_mountPathDirPicker->SetPath(wxFileConfig::Get()->Read(kMountPointSettingsKey));
+    wxGridSizer* bSizer4;
+    bSizer4 = new wxGridSizer(1, 0, 0);
+
+    m_panel5->SetSizer(bSizer4);
+    m_panel5->Layout();
+    bSizer4->Fit(m_panel5);
+    m_webView = wxWebView::New(m_panel5, wxID_ANY);
+    bSizer4->Add(m_webView, 0, wxALL | wxEXPAND);
+    m_webView->LoadURL("http://localhost:8080");
 }
 
 void LauncherAppMainFrame::OnStartStopButtonClick(wxCommandEvent& event) {
@@ -27,7 +36,7 @@ void LauncherAppMainFrame::OnStartStopButtonClick(wxCommandEvent& event) {
             m_statusBar1->SetStatusText("Running", 0);
             _dhfsInstance.start(wxFileConfig::Get()->Read(kMountPointSettingsKey).ToStdString(), {
 
-            });
+                                });
             break;
         default:
             throw Exception("Unhandled switch case");
