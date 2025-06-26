08f61ef9e49408132eefa69735cbe5c1ff6ccc63
Stepan Usatiuk <stepan@usatiuk.com>
2025-06-26 12:34:01 +0200

    reload web ui on start

--- launcher/gui/src/GLauncherApp.cpp
+++ launcher/gui/src/GLauncherApp.cpp
@@ -53,7 +53,7 @@ MainFrame::MainFrame( wxWindow* parent, wxWindowID id, const wxString& title, co
 	m_panel1->SetSizer( bSizer2 );
 	m_panel1->Layout();
 	bSizer2->Fit( m_panel1 );
-	m_notebook1->AddPage( m_panel1, _("Info"), false );
+	m_notebook1->AddPage( m_panel1, _("Info"), true );
 	m_panel3 = new wxPanel( m_notebook1, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
 	wxGridSizer* gSizer1;
 	gSizer1 = new wxGridSizer( 1, 1, 0, 0 );
@@ -65,7 +65,7 @@ MainFrame::MainFrame( wxWindow* parent, wxWindowID id, const wxString& title, co
 	m_panel3->SetSizer( gSizer1 );
 	m_panel3->Layout();
 	gSizer1->Fit( m_panel3 );
-	m_notebook1->AddPage( m_panel3, _("Logs"), true );
+	m_notebook1->AddPage( m_panel3, _("Logs"), false );
 	m_panel2 = new wxPanel( m_notebook1, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
 	wxBoxSizer* bSizer5;
 	bSizer5 = new wxBoxSizer( wxVERTICAL );
@@ -137,7 +137,7 @@ MainFrame::MainFrame( wxWindow* parent, wxWindowID id, const wxString& title, co
 	m_panel4 = new wxPanel( m_notebook1, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
 	m_notebook1->AddPage( m_panel4, _("Advanced Settings"), false );
 	m_panel5 = new wxPanel( m_notebook1, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
-	m_notebook1->AddPage( m_panel5, _("a page"), false );
+	m_notebook1->AddPage( m_panel5, _("Web UI"), false );
 
 	bSizer3->Add( m_notebook1, 1, wxEXPAND, 5 );
 
